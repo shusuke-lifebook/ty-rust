daee123d8c1283b505b145bcd02417a55be2ff8f
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-07-11 14:59:37 +0900

    コメントのサンプルを追加

---
+++ intro/src/bin/comment.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("Hello, world!");
+}
+/*
+println! // こんなコメントはNG ("Hello, World!!");
+// 今は無効 println!("Hello, World!!");
+*/
+
+/*
+ちょっと長めのコメントを書くとき、
+このような書き方をします。
+*/
+/*
+一時的に無効
+println!("Hello, World!!");
+println!("こんにちは、世界。");
+*/
+
+/* 単一行のコメントの替わり */
+
+/*
+println!("Hello, World!!");
+/*
+println!("こんにちは、世界。");
+*/
+println!("你好世界");
+*/

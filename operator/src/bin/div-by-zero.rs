1fdd0c320ed3358dfbe0a9c0bf019a4742372db9
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-08-16 08:47:33 +0900

    リスト3.15まで学習完了

--- operator/src/bin/div-by-zero.rs
+++ operator/src/bin/div-by-zero.rs
@@ -9,4 +9,11 @@ fn main() {
     // 実行時にゼロ除算でpanic
     let s = "0";
     let d = i32::from_str(s).unwrap();
+    // let r = 10 / d; // 実行時にpanic
+
+    // 浮動小数点によるゼロ除算
+    let f = 10.0;
+    let g = 0.0;
+    println!("{f} / {g} = {}", f / g);
+    println!("{f} % {g} = {}", f % g);
 }

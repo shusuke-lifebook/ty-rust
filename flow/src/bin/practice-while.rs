c14bd7a6d8e8bfe853f729eace652d1183618238
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-08-22 17:20:54 +0900

    4.2の練習問題の完了

---
+++ flow/src/bin/practice-while.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut i = 1;
+    while i < 10 {
+        let mut j = 1;
+        while j < 10 {
+            let result = i * j;
+            print!("{result:2} ");
+            j += 1;
+        }
+        println!();
+        i += 1;
+    }
+}

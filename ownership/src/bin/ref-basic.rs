2cbbe82b68d9540344a9baf97a848b3a809b3ba4
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-09-19 16:14:50 +0900

    リスト6.12まで学習完了

---
+++ ownership/src/bin/ref-basic.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut string1 = String::new();
+    string1 = "Hello, Rust!".to_string();
+    let string2 = String::from("こんにちは、Rust!");
+    println!("{string1}");
+    println!("{string2}");
+    let string3 = &string1;
+    println!("{string3}");
+    println!("{string1}");
+
+    let r = &123;
+    println!("{r}");
+}

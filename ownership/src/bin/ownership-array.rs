480b4611db5f7a8e5cbbc225f258c276fc8b2921
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-09-19 14:26:47 +0900

    リスト6.6まで学習完了

---
+++ ownership/src/bin/ownership-array.rs
@@ -0,0 +1,9 @@
+// コメントアウトを解除するとコンパイルエラーになります。
+fn main() {
+    let scalar_a = [1, 2, 3];
+    let scalar_b = scalar_a;
+    let sequence_a = [String::from("a"), String::from("b")];
+    let sequence_b = sequence_a;
+    println!("スカラー型：{},{}", scalar_a[0], scalar_b[0]);
+    // println!("シーケンス型：{},{}", sequence_a[0], sequence_b[0]);
+}

39bc9e8ae77051520fdfb3de9a48e471aab7dd8b
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-09-21 17:07:41 +0900

    リスト6.27まで学習完了

---
+++ ownership/src/bin/search-array-1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+    let n = 3;
+    let pos = search_position(&a, n);
+    println!("{n} は {a:?} の {} 番目にあります。", pos + 1);
+}
+
+fn search_position(a: &[i32; 5], n: i32) -> usize {
+    let pos = a.iter().position(|&x| x == n).unwrap();
+    pos
+}
